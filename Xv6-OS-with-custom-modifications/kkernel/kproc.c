e7648e8e82dacc00cddb22f7ea7ea2265d77d549
Erik M. Buck <ebuck1@udayton.edu>
2025-04-02 12:30:55 -0400

    experiment to determine if compiler will generate similar code for clarified scheduler() with -O3 optimization

--- Xv6-OS-with-custom-modifications/kkernel/kproc.c
+++ Xv6-OS-with-custom-modifications/kkernel/kproc.c
@@ -310,7 +310,6 @@ int wait(void) {
 //   - eventually that process transfers control
 //       via swtch back to the scheduler.
 void scheduler(void) {
-  struct proc *p;
   struct cpu *c = mycpu();
   c->proc = 0;
 
@@ -320,24 +319,24 @@ void scheduler(void) {
 
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
-    for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
-      if (p->state != RUNNABLE) {
-        continue;
+    for( int i = 0; i < NPROC; ++i) {
+      struct proc *p = &ptable.proc[i];
+      if (p->state == RUNNABLE) {
+        // Switch to chosen process.  It is the process's job
+        // to release ptable.lock and then reacquire it
+        // before jumping back to us.
+        c->proc = p;
+        switchuvm(p);
+        p->state = RUNNING;
+
+        swtch(&(c->scheduler), p->context);
+        switchkvm();
+
+        // Process is done running for now.
+        // It should have changed its p->state before coming back.
+        c->proc = 0;
       }
 
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
     }
     release(&ptable.lock);
   }
